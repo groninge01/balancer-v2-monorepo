ae193543d09872eddec3d47aa8613c622059da79
beethovenx <beethovenxfi@gmail.com>
2021-09-13 10:39:48 +0200

    Add testnet deployment for balancer helpers

--- deployments/migrations/005_helpers.ts
+++ deployments/migrations/005_helpers.ts
@@ -1,14 +1,23 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 
 export default async function (hre: HardhatRuntimeEnvironment): Promise<void> {
-  const { deployments, getNamedAccounts } = hre;
+  const { deployments, getNamedAccounts, getChainId } = hre;
   const { deploy } = deployments;
+  const chainId = await getChainId();
 
   const { deployer } = await getNamedAccounts();
 
-  await deploy('BalancerHelpers', {
-    from: deployer,
-    args: ['0x20dd72Ed959b6147912C2e529F0a0C651c33c9ce'],
-    log: true,
-  });
+  if (chainId === '4002') {
+    await deploy('BalancerHelpers', {
+      from: deployer,
+      args: ['0x974D3FF709D84Ba44cde3257C0B5B0b14C081Ce9'],
+      log: true,
+    });
+  } else if (chainId === '250') {
+    await deploy('BalancerHelpers', {
+      from: deployer,
+      args: ['0x20dd72Ed959b6147912C2e529F0a0C651c33c9ce'],
+      log: true,
+    });
+  }
 }
