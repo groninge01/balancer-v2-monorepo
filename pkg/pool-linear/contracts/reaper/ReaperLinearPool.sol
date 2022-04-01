50644db8da53571f6331933111a16fad385021ec
Magnus Brantheim <m.brantheim@gmail.com>
2022-04-01 22:21:24 +0200

    Add Reaper boosted pool code

---
+++ pkg/pool-linear/contracts/reaper/ReaperLinearPool.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "../interfaces/IReaperTokenVault.sol";
+
+import "../LinearPool.sol";
+
+contract ReaperLinearPool is LinearPool {
+    IReaperTokenVault private immutable _tokenVault;
+
+    constructor(
+        IVault vault,
+        string memory name,
+        string memory symbol,
+        IERC20 mainToken,
+        IERC20 wrappedToken,
+        uint256 upperTarget,
+        uint256 swapFeePercentage,
+        uint256 pauseWindowDuration,
+        uint256 bufferPeriodDuration,
+        address owner
+    )
+        LinearPool(
+            vault,
+            name,
+            symbol,
+            mainToken,
+            wrappedToken,
+            upperTarget,
+            swapFeePercentage,
+            pauseWindowDuration,
+            bufferPeriodDuration,
+            owner
+        )
+    {
+        _tokenVault = IReaperTokenVault(address(wrappedToken));
+        _require(address(mainToken) == address(IReaperTokenVault(address(wrappedToken))), Errors.TOKENS_MISMATCH);
+    }
+
+    function _getWrappedTokenRate() internal view override returns (uint256) {
+        return _tokenVault.getPricePerFullShare();
+    }
+}
